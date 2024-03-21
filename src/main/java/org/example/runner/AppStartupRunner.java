73d5d03d6ce23562bee9f16d69bf0fc4063ff96c
선도형/협업서비스개발블루팀/ND <dohhyung.seon@nhndooray.com>
2024-03-21 17:42:09 +0900

    - spring SpEL

---
+++ src/main/java/org/example/runner/AppStartupRunner.java
@@ -0,0 +1,48 @@
+package org.example.runner;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppStartupRunner implements ApplicationRunner {
+
+
+    @Value("${greeting.english}+${greeting.korean}")
+    private String value1;
+
+    @Value("${greeting.english}" + "${greeting.korean}")
+    private String value2;
+
+    @Value("${number.one}${number.two}")
+    private String value3;
+
+    @Value("${number.one}" + "${number.two}")
+    private String value4;
+
+    @Value("${number.one}" + "${number.two}")
+    private int value5;
+
+    @Value("#{1+2}")
+    private String value6;
+
+    @Value("#{${number.one} + ${number.two}}")
+    private int value7;
+
+    @Value("#{${number.one} eq 1}")
+    private boolean value8;
+
+    @Value("#{${number.one} eq \"1\"}")
+    private boolean value9;
+
+    @Value("#{${number.one} + ${number.two} + (${number.three} ?: 4)}")
+    private int value10;
+
+
+    //TODO-2 하나씩 찍어가면서 예상하는 답이 나오는지 확인한다.
+    @Override
+    public void run(ApplicationArguments args) {
+        System.out.println(value1);
+    }
+}
