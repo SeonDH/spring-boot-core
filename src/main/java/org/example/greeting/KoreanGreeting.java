76e19dc3d66d0d4868d860dd30c5e601e6a89899
선도형/협업서비스개발블루팀/ND <dohhyung.seon@nhndooray.com>
2024-03-21 17:42:09 +0900

    - greeting

---
+++ src/main/java/org/example/greeting/KoreanGreeting.java
@@ -0,0 +1,12 @@
+package org.example.greeting;
+
+
+public class KoreanGreeting implements Greeting {
+
+    @Override
+    public void sayHello() {
+        System.out.println("안녕 세상");
+    }
+
+}
+
