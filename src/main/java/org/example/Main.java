4f70ec55bb9f78460ef05bc4105490057be86018
선도형/협업서비스개발블루팀/ND <dohhyung.seon@nhndooray.com>
2024-03-21 10:42:14 +0900

    - factory pattern

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,20 @@
 package org.example;
 
+//TODO-1 main 에서 직접 Shape 를 객체를 생성하지 않고, ShapeFactory 에 객체 생성을 위임한다.
 public class Main {
     public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+
+        Shape shape1 = shapeFactory.getShape("CIRCLE");
+
+        shape1.draw();
+
+        Shape shape2 = shapeFactory.getShape("RECTANGLE");
+
+        shape2.draw();
+
+        Shape shape3 = shapeFactory.getShape("SQUARE");
+
+        shape3.draw();
     }
 }
---
+++ src/main/java/org/example/ShapeFactory.java
@@ -0,0 +1,22 @@
+package org.example;
+
+//TODO-2 factory 에서 객체의 생성을 위임 받는다.
+public class ShapeFactory {
+
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+
+        return null;
+    }
+}
